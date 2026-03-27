81539ff9433b0b6a608ebc3ba2597f6afd69ccfb
MoffittCaspiLab <cwk20@duke.edu>
2026-03-27 16:34:39 -0400

    Delete R/pwd_creation.R
    This has been set to .gitignore

--- R/pwd_creation.R
+++
@@ -1,8 +0,0 @@
-library(sodium)
-# Create our initial hashed value
-HASH_FILE <- "password.txt"
-if (!file.exists(HASH_FILE)) {
-  hashed <- sodium::password_store("AgeOld") # strong salted hash
-  writeLines(hashed, HASH_FILE)
-  try(Sys.chmod(HASH_FILE, "0600"), silent = TRUE) # best-effort restrictive perms
-}
